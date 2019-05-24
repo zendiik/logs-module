64659a665994abeead5f436b08b05d317cbc0088
Jan Ženíšek <zenisek@oksystem.cz>
2019-05-24 09:42:22 +0200

    Opravena cesta k assets a přidáno mazání starých assetů

--- src/assets/public/post-install.js
+++ src/assets/public/post-install.js
@@ -2,10 +2,11 @@
 
 let gentlyCopy = require('gently-copy')
 const fs = require('fs')
+const path = require('path')
 
 const filesTocopy = ['css', 'js']
-const wwwPath = process.env.INIT_CWD + '/www'
-const assetsPath = process.env.INIT_CWD + '/www/assets'
+const wwwPath = path.resolve(process.cwd(), '../../../www')
+const assetsPath = path.resolve(process.cwd(), '../../../www/assets')
 
 if (!fs.existsSync(wwwPath)) {
 	fs.mkdirSync(wwwPath)
@@ -13,6 +14,12 @@ if (!fs.existsSync(wwwPath)) {
 
 if (!fs.existsSync(assetsPath)) {
 	fs.mkdirSync(assetsPath)
+} else {
+	fs.readdirSync(assetsPath).forEach(dir => {
+		fs.readdirSync(assetsPath + '/' + dir).forEach(file => {
+			fs.unlinkSync(assetsPath + '/' + dir + '/' + file)
+		})
+	})
 }
 
 gentlyCopy(filesTocopy, assetsPath)
